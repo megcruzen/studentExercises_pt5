8fcdfd630a496702e8a30ec4f7f0f4af7e8d18d0
Meg Cruzen <megcruzen@gmail.com>
2019-03-25 15:34:39 -0500

    First commit

---
+++ StudentExercises/Models/Exercise.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExercises.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string ExerciseName { get; set; }
+        public string ExerciseLanguage { get; set; }
+    }
+}
